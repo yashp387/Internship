2d400100c2f103d384cbc74ef0db24c7e52c364c
yashp387 <yashmp2004@gmail.com>
2025-05-29 11:40:30 +0530

    dotnet learning

---
+++ dotnet_learning-Day3/Controllers/BookController.cs
@@ -0,0 +1,72 @@
+﻿using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : Controller
+    {
+        private readonly BookService bookService;
+
+        public BookController(BookService book)
+        {
+            this.bookService = book;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public ActionResult AddBook(Book book)
+        {
+            this.bookService.AddBook(book);
+            return Ok("Book created!");
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult GetAll()
+        {
+            return Ok(this.bookService.GetAll());
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public ActionResult GetById(int id)
+        {
+            var res = this.bookService.GetBookById(id);
+
+            if (res != null) { return Ok(res); }
+
+            return NotFound("Book not found!");
+        }
+
+        //Update Book
+        [HttpPut]
+        [Route("Update")]
+        public ActionResult UpdateBook(int id, Book updatedBook)
+        {
+            var success = this.bookService.UpdateBook(id, updatedBook);
+            if (!success)
+            {
+                return NotFound("Book not found!");
+            }
+
+            return Ok("Book updated!");
+        }
+
+        //Delete Book
+        [HttpDelete]
+        [Route("Delete")]
+        public ActionResult DeleteBook(int id)
+        {
+            var success = this.bookService.DeleteBook(id);
+            if (!success)
+            {
+                return NotFound("Book not found!");
+            }
+
+            return Ok("Book deleted!");
+        }
+    }
+}
---
+++ dotnet_learning-Day3/services/BookService.cs
@@ -0,0 +1,53 @@
+﻿using BooksApi.Models;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private List<Book> _books;
+
+        public BookService()
+        {
+            _books = new List<Book>();
+        }
+
+        // To Add Book
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        // To Get All Books
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+
+        // To Get Single Book
+        public Book? GetBookById(int id)
+        {
+            return _books.Find(x => x.Id == id);
+        }
+
+        //  To Update Book
+        public bool UpdateBook(int id, Book updatedBook)
+        {
+            var index = _books.FindIndex(x => x.Id == id);
+            if (index == -1) return false;
+
+            updatedBook.Id = id; // Make sure the ID stays the same
+            _books[index] = updatedBook;
+            return true;
+        }
+
+        //  To Delete Book
+        public bool DeleteBook(int id)
+        {
+            var book = _books.Find(x => x.Id == id);
+            if (book == null) return false;
+
+            _books.Remove(book);
+            return true;
+        }
+    }
+}
